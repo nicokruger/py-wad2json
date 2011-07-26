51c7d9b56e1a52f2c638098bb42e8f48e09ece5d
Nico Kruger <nico.kruger@rorotika.com>
2011-07-26 09:23:19 +0200

    Initial checkin of our little WAD to Javascript parser.

---
+++ a.py
@@ -0,0 +1,82 @@
+import mapedit
+import wad
+import collections
+
+w = wad.WAD("doom.wad")
+e1m1 = w.maps['E1M1']
+
+
+sectors_sidedefs = collections.defaultdict(list)
+
+m = mapedit.MapEditor(e1m1)
+
+[sectors_sidedefs[sidedef.sector].append((i,sidedef)) for i,sidedef in enumerate(m.sidedefs)]
+
+def find_linedef_from_sidedef(linedefs, sidedef):
+	return [l for l in linedefs if (l.front == sidedef) or (l.back == sidedef)][0]
+
+sectors_linedefs = collections.defaultdict(list)
+
+#[sectors_linedefs[sector].append(find_linedef_from_sidedef(m.linedefs, sidedef[0])) for sidedef in sectors_sidedefs[sector] for sector in sectors_sidedefs.keys()]
+
+[sectors_linedefs[sector].append(find_linedef_from_sidedef(m.linedefs, sidedef[0])) for sector in sectors_sidedefs.keys() for sidedef in sectors_sidedefs[sector]]
+
+def sigh(l, item):
+	try:
+		l.index(item)
+	except:
+		return -1
+
+class WrappedLinedef:
+	def __init__(self, vx_a, vx_b):
+		self.vx_a = vx_a
+		self.vx_b = vx_b
+
+def order_linedefs(linedefs):
+	
+	edge_links_forward = {}
+	edge_links_backward = {}
+
+	vertices = [];
+	for l in linedefs:
+		edge_links_forward[l.vx_a] = l.vx_b
+		edge_links_backward[l.vx_b] = l.vx_a
+		vertices.append(l.vx_a)
+		vertices.append(l.vx_b)
+	
+	new_edges = []
+	v = linedefs[0].vx_b
+	visited = []
+	# Rewind
+	while (v in edge_links_backward) and (not v in visited):
+		visited.append(v)
+		v = edge_links_backward[v]
+	
+	end = edge_links_forward[v]
+
+	finished = []
+	while end != None and not end in finished:
+		new_linedef = WrappedLinedef(v, end)
+		finished.append(v)
+		finished.append(end)
+
+		new_edges.append(new_linedef)
+		tmp = end
+		v = end
+		try:
+			end = edge_links_forward[tmp]
+		except:
+			end = None
+
+	return new_edges
+
+for sector,linedefs in sectors_linedefs.iteritems():
+	print "SECTOR:", sector
+	#for linedef in order_linedefs(linedefs):
+	for linedef in linedefs:
+		vx_a = m.vertexes[linedef.vx_a]
+		vx_b = m.vertexes[linedef.vx_b]
+
+		print "   LINEDEF: %.2f,%.2f -> %.2f,%.2f" % (vx_a.x, vx_a.y, vx_b.x, vx_b.y)
+
+
