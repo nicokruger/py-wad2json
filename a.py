ba7d9ea9975225a02c9c5ffcc0411b6725166dc4
Nico Kruger <nico.kruger@rorotika.com>
2011-08-21 14:33:44 +0200

    Fixed bug where last vertices are missed.

--- a.py
+++ a.py
@@ -36,6 +36,7 @@ textures = {}
 sectors_linedefs = collections.defaultdict(list)
 
 for sector,sidedefs in sectors_sidedefs.iteritems():
+	sidedefs = sectors_sidedefs[sector]
 	textures[sector] = m.sectors[sector].tx_floor.lower()
 	for sidedef in sidedefs:
 		sectors_linedefs[sector].append(find_linedef_from_sidedef(m.linedefs, sidedef[0]))
@@ -51,22 +52,22 @@ def order_linedefs(linedefs):
 		edge_links_backward[l.vx_b] = l.vx_a
 		vertices.append(l.vx_a)
 		vertices.append(l.vx_b)
-	
-	new_edges = []
+
 	v = linedefs[0].vx_b
 	visited = []
+
 	# Rewind
 	while (v in edge_links_backward) and (not v in visited):
 		visited.append(v)
 		v = edge_links_backward[v]
-	
-	end = edge_links_forward[v]
 
+	end = edge_links_forward[v]
+	START = [v,end]
 	finished = []
-	while end != None and not end in finished:
+	new_edges = []
+	while end != None and not [v,end] in finished:
 		new_linedef = WrappedLinedef(v, end)
-		finished.append(v)
-		finished.append(end)
+		finished.append([v,end])
 
 		new_edges.append(new_linedef)
 		tmp = end
@@ -88,9 +89,7 @@ def reverse_linedefs(linedefs):
 	return reversed_linedefs
 
 for sector,linedefs in sectors_linedefs.iteritems():
-	print "SECTOR:", sector
 	for linedef in order_linedefs(linedefs):
-	#for linedef in linedefs:
 		vx_a = m.vertexes[linedef.vx_a]
 		vx_b = m.vertexes[linedef.vx_b]
 
@@ -108,12 +107,13 @@ print "----------------------------------------------"
 json = '{ "sectors": ['
 polygons = []
 for sector,linedefs in sectors_linedefs.iteritems():
+#for sector in [45]:
+	linedefs = sectors_linedefs[sector]
 	points =  ",".join(["[%.2f,%.2f]" % (scalex(m.vertexes[l.vx_a].x), scaley(m.vertexes[l.vx_a].y)) for l in reverse_linedefs(order_linedefs(linedefs))]),
 	polygons.append([points,textures[sector]])
 
 json += ",".join([' {"points" : [%s], "texture" : "%s", "label":"%s" }' % (p[0][0],p[1],"polygon"+str(i)) for i,p in enumerate(polygons)])
 
-	#print ", $V($.2f,%.2f)" % (m.vertexes[order_linedefs(linedefs)[-1].vx_b].x, m.vertexes[order_linedefs(linedefs)[-1].vx_b].y),
 json += "]}"
 print json
 
