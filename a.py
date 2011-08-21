f90c82b5e7dc7c076ba9d31bb1ea62c4ecca4ad0
Nico Kruger <nico.kruger@rorotika.com>
2011-08-21 15:11:58 +0200

    Refactored code for easier reading.
    Original script was basically just a quick hack, one statement per line
    script to prove a point. Refactored it a bit to make it easier to work
    with.

--- a.py
+++ a.py
@@ -14,34 +14,71 @@ class WrappedLinedef:
 		self.vx_a = vx_a
 		self.vx_b = vx_b
 
-w = wad.WAD("doom.wad")
-e1m1 = w.maps['E1M1']
-m = mapedit.MapEditor(e1m1)
+class DoomExporter:
+	def __init__(self,wadfile,mapname, outfile):
+		self.w = wad.WAD(wadfile)
+		self.mapentry = self.w.maps[mapname]
 
-sectors_sidedefs = collections.defaultdict(list)
-
-for i,sidedef in enumerate(m.sidedefs):
-	sectors_sidedefs[sidedef.sector].append((i,sidedef))
-
-
-def find_linedef_from_sidedef(linedefs, sidedef):
-	for linedef in linedefs:
-		if linedef.front == sidedef:
-			return linedef;
-		elif linedef.back == sidedef:
-			l = WrappedLinedef(linedef.vx_b, linedef.vx_a)
-			return l
+		self.m = mapedit.MapEditor(self.mapentry)
 		
-textures = {}
-sectors_linedefs = collections.defaultdict(list)
+		self.export(outfile)
 
-for sector,sidedefs in sectors_sidedefs.iteritems():
-	sidedefs = sectors_sidedefs[sector]
-	textures[sector] = m.sectors[sector].tx_floor.lower()
-	for sidedef in sidedefs:
-		sectors_linedefs[sector].append(find_linedef_from_sidedef(m.linedefs, sidedef[0]))
+	def scalex(self,x):
+		return x + 800
+	def scaley(self,y):
+		return y + 4500
 
-def order_linedefs(linedefs):
+	def export(self, outfile):
+		textures,sectors_linedefs = self.__getlinedefs_per_sector(self.m, self.__getsidedefs_per_sector(self.m))
+		for sector,linedefs in sectors_linedefs.iteritems():
+			for linedef in LinedefOrder(linedefs):
+				vx_a = self.m.vertexes[linedef.vx_a]
+				vx_b = self.m.vertexes[linedef.vx_b]
+
+				print "   LINEDEF: %.2f,%.2f -> %.2f,%.2f" % (vx_a.x, vx_a.y, vx_b.x, vx_b.y)
+
+		self.textures = set(textures.values())	
+
+		self.json = '{ "sectors": ['
+		polygons = []
+		for sector,linedefs in sectors_linedefs.iteritems():
+			linedefs = sectors_linedefs[sector]
+			points =  ",".join(["[%.2f,%.2f]" % (self.scalex(self.m.vertexes[l.vx_a].x), self.scaley(self.m.vertexes[l.vx_a].y)) for l in ReverseLinedefs(LinedefOrder(linedefs))]),
+			polygons.append([points,textures[sector]])
+		self.json += ",".join([' {"points" : [%s], "texture" : "%s", "label":"%s" }' % (p[0][0],p[1],"polygon"+str(i)) for i,p in enumerate(polygons)])
+		self.json += "]}"
+
+
+
+	# create linkage between from sector to sidedef
+	def __getsidedefs_per_sector(self, m):
+
+		sectors_sidedefs = collections.defaultdict(list)
+
+		for i,sidedef in enumerate(m.sidedefs):
+			sectors_sidedefs[sidedef.sector].append((i,sidedef))
+		return sectors_sidedefs
+
+	def __getlinedefs_per_sector(self, m, sectors_sidedefs):
+		textures = {}
+		sectors_linedefs = collections.defaultdict(list)
+		for sector,sidedefs in sectors_sidedefs.iteritems():
+			sidedefs = sectors_sidedefs[sector]
+			textures[sector] = m.sectors[sector].tx_floor.lower()
+			for sidedef in sidedefs:
+				sectors_linedefs[sector].append(self.__find_linedef_from_sidedef(m.linedefs, sidedef[0]))
+		
+		return textures,sectors_linedefs
+
+	def __find_linedef_from_sidedef(self, linedefs, sidedef):
+		for linedef in linedefs:
+			if linedef.front == sidedef:
+				return linedef;
+			elif linedef.back == sidedef:
+				l = WrappedLinedef(linedef.vx_b, linedef.vx_a)
+				return l
+
+def LinedefOrder(linedefs):
 	
 	edge_links_forward = {}
 	edge_links_backward = {}
@@ -79,7 +116,7 @@ def order_linedefs(linedefs):
 
 	return new_edges
 
-def reverse_linedefs(linedefs):
+def ReverseLinedefs(linedefs):
 	linedefscopy = list(linedefs)
 	linedefscopy.reverse()
 	reversed_linedefs = []
@@ -88,37 +125,19 @@ def reverse_linedefs(linedefs):
 		reversed_linedefs.append(l)
 	return reversed_linedefs
 
-for sector,linedefs in sectors_linedefs.iteritems():
-	for linedef in order_linedefs(linedefs):
-		vx_a = m.vertexes[linedef.vx_a]
-		vx_b = m.vertexes[linedef.vx_b]
+if __name__ == "__main__":
+	de = DoomExporter("doom.wad", "E1M1", outfile)
 
-		print "   LINEDEF: %.2f,%.2f -> %.2f,%.2f" % (vx_a.x, vx_a.y, vx_b.x, vx_b.y)
+	
+	print "----------------------------------------------"
+	print " JSON "
+	print "----------------------------------------------"
+	print de.json
+	open(outfile,"w").write(de.json)
 
-def scalex(x):
-	return x + 800
-def scaley(y):
-	return y + 4500
+	print "--------"
+	print "Textures"
+	print "--------"
+	print de.textures
 
-print "----------------------------------------------"
-print " JSON "
-print "----------------------------------------------"
-
-json = '{ "sectors": ['
-polygons = []
-for sector,linedefs in sectors_linedefs.iteritems():
-#for sector in [45]:
-	linedefs = sectors_linedefs[sector]
-	points =  ",".join(["[%.2f,%.2f]" % (scalex(m.vertexes[l.vx_a].x), scaley(m.vertexes[l.vx_a].y)) for l in reverse_linedefs(order_linedefs(linedefs))]),
-	polygons.append([points,textures[sector]])
-
-json += ",".join([' {"points" : [%s], "texture" : "%s", "label":"%s" }' % (p[0][0],p[1],"polygon"+str(i)) for i,p in enumerate(polygons)])
-
-json += "]}"
-print json
-
-open(outfile,"w").write(json)
-
-print "Textures"
-print "--------"
-print set(textures.values())
+	print de.textures
